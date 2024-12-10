97adfa9d72cf76366a707992b421adfcc8103023
Lucas Leite <me@lucasleite.us>
2024-12-09 21:29:29 -0500

    add tsconfig.jest.json

--- jest.config.ts
+++ jest.config.ts
@@ -176,7 +176,10 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      { tsconfig: "<rootDir>/tsconfig.jest.json" },
+    ],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
